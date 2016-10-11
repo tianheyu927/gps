6987e4791eb3a349719919ccd0225c05bef9e367
Justin Fu <justinjfu@gmail.com>
2016-10-11 18:25:35 +0900

    Add copy into algorithm

--- python/gps/algorithm/policy_opt/tf_model_example.py
+++ python/gps/algorithm/policy_opt/tf_model_example.py
@@ -211,7 +211,7 @@ def multi_modal_network_fp(dim_input=27, dim_output=7, batch_size=25, network_co
     first_dense_size = conv_out_size + len(x_idx)
 
     # Store layers weight & bias
-    with tf.variable_scope('conv'):
+    with tf.variable_scope('conv_params'):
         weights = {
             'wc1': init_weights([filter_size, filter_size, num_channels, num_filters[0]]), # 5x5 conv, 1 input, 32 outputs
             'wc2': init_weights([filter_size, filter_size, num_filters[0], num_filters[1]]), # 5x5 conv, 32 inputs, 64 outputs
